28caf9140dbe8f0ba91d806c6030528ddbc88d80
Heiko Koehler <heiko@pernixdata.com>
2016-09-05 22:06:31 -0700

    concurrent traversal using go routines

--- find.go
+++ find.go
@@ -21,37 +21,71 @@ type Options struct {
 	num   bool   // number of files
 }
 
-func find(opts Options) error {
+// Result of directoy traversal
+type Result struct {
+	du  int64
+	num uint64
+}
+
+// impose global limit on open files
+var openFileSem chan int
+
+// report result back to parent via comp channel
+func find(opts Options, comp chan *Result) {
+	res := new(Result)
+	count := 0
+	subChan := make(chan *Result)
+
+	// acquire open file
+	<-openFileSem
 	dir, err := os.Open(opts.path)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
+
 	for {
 		files, err := dir.Readdir(512)
 		switch err {
 		case io.EOF:
-			return nil
+			goto done
 		case nil:
+			break
 		default:
 			log.Fatal(err)
-			return err
 		}
+
+		// spawn gopher for each sub directory
 		for _, file := range files {
 			filePath := filepath.Join(opts.path, file.Name())
-			// descent into directory
-			if file.Mode().IsDir() {
-				newOpts := opts
-				newOpts.path = filePath
-				err = find(newOpts)
-				if err != nil {
-					return err
-				}
-			}
 			if opts.print {
 				fmt.Println(filePath)
 			}
+			res.du += file.Size() // XXX size not disk usage
+			res.num++
+			if file.Mode().IsDir() {
+				newOpts := opts
+				newOpts.path = filePath
+				count++
+				go find(newOpts, subChan)
+			}
 		}
 	}
+
+done:
+	// wait for gophers to complete
+	// update total result with results from gophers
+	for count > 0 {
+		subRes := <-subChan
+		res.du += subRes.du
+		res.num += subRes.num
+		count--
+	}
+
+	// signal parent gopher
+	comp <- res
+
+	// release open file
+	openFileSem <- 1
 }
 
 func main() {
@@ -72,8 +106,22 @@ func main() {
 	} else {
 		opts.path = args[0]
 	}
-	err := find(opts)
-	if err != nil {
-		log.Fatal(err)
+
+	// limit number of open files to 1000
+	openFileSem = make(chan int, 1000)
+	for i := 0; i < 1000; i++ {
+		openFileSem <- 1
+	}
+
+	// go gopher go!
+	wait := make(chan *Result)
+	go find(opts, wait)
+	res := <-wait
+
+	if opts.num {
+		fmt.Printf("%d files\n", res.num)
+	}
+	if opts.du {
+		fmt.Printf("%d bytes\n", res.du)
 	}
 }
